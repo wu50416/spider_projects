9ba434c7bfebfe8543cf9638a8d54446f23bc6d6
wu50416 <504168539@qq.com>
2023-04-04 16:55:33 +0800

    人民法院诉讼网

---
+++ 法拍网其他系列/RMFYSS_Script/airflow_test/Airflow_RMFYSS_Detail.py
@@ -0,0 +1,22 @@
+import pytz
+from airflow import DAG
+from airflow.contrib.operators.ssh_operator import SSHOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    "owner": "airflow",
+    "email": ["wangdong@iunispace.com"],
+    "email_on_failure": True,
+}
+tz = pytz.timezone('Asia/Shanghai')
+dt = datetime(2022, 11, 1, tzinfo=tz)
+utc_dt = dt.astimezone(pytz.utc).replace(tzinfo=None)
+# 00 01-23/1   01~23点，每小时一次
+with DAG(
+        dag_id="Spider_RMFYSS_Detail", catchup=False, default_args=default_args, tags=['spider'],
+        start_date=utc_dt, max_active_runs=1, schedule_interval='00 01-23/1 * * *') as dag:
+    SpiderBD = SSHOperator(ssh_conn_id="ssh.192.168.1.119.wangdong",
+                           task_id="RMFYSS_Detail",
+                           command="cd /home/wangdong/fp_spider/RMFYSS_Script/; /home/wangdong/anaconda3/envs/python3.9.5/bin/python -u Crawl_RMFYSS_Detail.py",
+                           dag=dag
+                           )
